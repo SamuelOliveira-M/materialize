f2db8c2147381f57ea051c76579a61dd17d89e85
samueloliveira_m <samuel098tr@gmail.com>
2023-06-13 14:33:31 -0300

    Correção de erros

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,4 @@
-const minhaSenha = 12345;
+const senhaCadastrada = 12345;
 const fullName = 'samuel';
 
 const formLogin = document.querySelector('.loginFields');
@@ -7,12 +7,15 @@ formLogin.addEventListener("submit" ,function(e){
 	e.preventDefault();
 
 	var name = e.target.querySelector('#full-name').value;
-	var name = e.target.querySelector('#email').value;
-	var name = e.target.querySelector('#password').value;
-	
-	if(name==fullName){
-		
+	var senha = e.target.querySelector('#password').value;
+	var botaoClicado = e.submitter.name;
+	if(botaoClicado="Login"){	
+		if(name==fullName  && senhaCadastrada == senha){
+			window.location.href = "https://samueloliveira-m.github.io/materialize/";
+		}	
+		else{
+			alert("Invalido")
+		}
 	}
-	
 
 })
